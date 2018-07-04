f93c99cbcf739a534a401d24d9dd3ee34e518163
homerooliveira <homero.santos.001@acad.pucrs.br>
2018-07-04 02:14:41 -0300

    implemented canvas view

---
+++ Draw/Model/DataBaseAcess.swift
@@ -0,0 +1,24 @@
+//
+//  DataBaseAcess.swift
+//  Draw
+//
+//  Created by Homero Oliveira on 04/07/18.
+//  Copyright © 2018 Homero Oliveira. All rights reserved.
+//
+
+import Foundation
+
+class DataBaseAcess {
+    
+    static let share = DataBaseAcess()
+    
+    private init() {}
+    
+    func changes(completion: ([DrawPath]) -> Void ) {
+        
+    }
+    
+    func save(_ drawPath: DrawPath) {
+        
+    }
+}
---
+++ Draw/View/DrawView.swift
@@ -0,0 +1,112 @@
+//
+//  DrawView.swift
+//  Draw
+//
+//  Created by Homero Oliveira on 04/07/18.
+//  Copyright © 2018 Homero Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    var canvasView: CanvasView!
+    var drawPath = DrawPath(isCompleted: false, color: "black", points: [])
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        canvasView = CanvasView(frame: .zero)
+        
+        DataBaseAcess.share.changes { (paths) in
+            DispatchQueue.main.async { [weak self] in
+                guard let strongSelf = self else { return }
+                if paths.isEmpty {
+                    strongSelf.canvasView.clearCanvas()
+                } else {
+                    strongSelf.canvasView.paths = paths
+                    strongSelf.canvasView.setNeedsDisplay()
+                }
+            }
+        }
+        
+        becomeFirstResponder()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let maxDimension = max(bounds.width, bounds.height)
+        var frame = canvasView.frame
+        frame.size.width = maxDimension
+        frame.size.height = maxDimension
+        frame.origin.x = (bounds.width - maxDimension) * 0.5
+        canvasView.frame = frame
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touchLocation = touches.first else { return }
+        let point = touchLocation.location(in: canvasView)
+        
+        drawPath.points.append(point)
+        
+        DataBaseAcess.share.save(drawPath)
+        canvasView.setNeedsDisplay()
+    }
+    
+    func add(point: CGPoint) {
+        drawPath.points.append(point)
+        DataBaseAcess.share.save(drawPath)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touchLocation = touches.first else { return }
+        let point = touchLocation.location(in: canvasView)
+        
+        add(point: point)
+        
+        canvasView.setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touchLocation = touches.first else { return }
+        let point = touchLocation.location(in: canvasView)
+        
+        add(point: point)
+        
+        drawPath.isCompleted = true
+        DataBaseAcess.share.save(drawPath)
+        drawPath = DrawPath(isCompleted: false, color: "black", points: [])
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touchesEnded(touches, with: event)
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+    }
+}
+//    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Reset Canvas?"
+//        message:@"This will clear the Realm database and reset the canvas. Are you sure you wish to proceed?"
+//        preferredStyle:UIAlertControllerStyleAlert];
+//
+//    typeof(self) __weak weakSelf = self;
+//    [alertController addAction:[UIAlertAction actionWithTitle:@"Reset"
+//        style:UIAlertActionStyleDefault
+//        handler:^(UIAlertAction *action) {
+//        [[RLMRealm defaultRealm] transactionWithBlock:^{
+//        [[RLMRealm defaultRealm] deleteAllObjects];
+//        }];
+//
+//        [weakSelf.canvasView clearCanvas];
+//        }]];
+//    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+//
+//    [[[[UIApplication sharedApplication] keyWindow] rootViewController] presentViewController:alertController animated:YES completion:nil];
+//}
